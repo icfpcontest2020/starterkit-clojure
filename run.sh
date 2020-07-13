0dccdd6f283ec99ff27d9c503d0727d84fe4d205
Nikita Prokopov <niki@tonsky.me>
2020-07-14 00:17:21 +0200

    httpkit for http requests

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-clojure -m main "$@"
+clojure -Srepro -m main "$@"
--- src/main.clj
+++ src/main.clj
@@ -1,4 +1,10 @@
-(ns main)
+(ns main
+  (:require 
+   [org.httpkit.client :as http]))
 
-(defn -main [[server-url player-key]]
-  (slurp (str server-url "?playerKey=" player-key)))
+(defn -main [server-url player-key]
+  (let [resp @(http/get server-url {:query-params {:playerKey "player-key"}})
+        {:keys [status headers body error]} resp]
+    (if error
+      (println "Unexpected server response:\nHTTP code:" status "\nResponse body:" body)
+      (println "Server response:" body))))
